cff6904f04cd4c9e22570a92b48adedc6e964333
LuisRemolina <luischuscano1@gmail.com>
2022-09-06 16:42:42 -0500

    Initial commit

---
+++ src/main/java/com/example/obspringsecuritycifrado/ObSpringSecurityCifradoApplication.java
@@ -0,0 +1,13 @@
+package com.example.obspringsecuritycifrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObSpringSecurityCifradoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObSpringSecurityCifradoApplication.class, args);
+    }
+
+}
